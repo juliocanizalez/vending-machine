9c2022c94868275bec9c44ee3d26698f3ae89671
Julio Eduardo Canizalez Salinas <juliocanizalez@outlook.com>
2021-03-13 23:43:26 -0600

    :hammer: Add more components and hook

---
+++ src/components/ItemsDispenser.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ItemsDispenser = () => {
+    return (
+        <div className='row'>
+            <div className="col-md-4">
+                
+            </div>
+        </div>
+    )
+}
+
+export default ItemsDispenser;
