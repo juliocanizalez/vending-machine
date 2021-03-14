dfef468c2466f5c8b3f5a8fcc1892bbe9c278a16
Julio Eduardo Canizalez Salinas <juliocanizalez@outlook.com>
2021-03-13 23:39:16 -0600

    :hammer: add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/styles/components/Header.scss'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <h1 className='text-center'>Vending Machine</h1>
+        </header>
+    )
+}
+
+export default Header
