649e9f8f975fec2df880b03e55f50a2f6e7e59ec
Julio Eduardo Canizalez Salinas <juliocanizalez@outlook.com>
2021-03-14 00:27:31 -0600

    :hammer: update component with basic structure

--- src/components/ItemsDispenser.jsx
+++ src/components/ItemsDispenser.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const ItemsDispenser = () => {
+    
+
     return (
         <div className='row'>
-            <div className="col-md-4">
-                
+            <div className="col-md-4 shadow ms-3">
+
+            </div>
+            <div className="col-md-4 shadow">
+
             </div>
         </div>
-    )
+    );
 }
 
 export default ItemsDispenser;
