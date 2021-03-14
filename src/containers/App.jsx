d7d7a33eed9daf6a560eb8d9e54187b269c27f55
Julio Eduardo Canizalez Salinas <juliocanizalez@outlook.com>
2021-03-14 00:28:04 -0600

    :hammer: add components to App.jsx

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
+import MainContainer from '../components/MainContainer.jsx';
+import ItemsDispenser from '../components/ItemsDispenser.jsx';
+
 import '../assets/styles/App.scss';
 
 const App = () => {
     return (
         <div className='App'>
             <Header />
+            <MainContainer>
+                <ItemsDispenser />
+            </MainContainer>
+            <Footer />
         </div>
     );
 }
