f42fb253249b3c908f2188aaf04961c096b098f2
Julio Eduardo Canizalez Salinas <juliocanizalez@outlook.com>
2021-03-14 16:50:21 -0600

    :hammer: Update components call

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
-import MainContainer from '../components/MainContainer.jsx';
 import ItemsDispenser from '../components/ItemsDispenser.jsx';
 
 import '../assets/styles/App.scss';
@@ -10,9 +9,7 @@ const App = () => {
     return (
         <div className='App'>
             <Header />
-            <MainContainer>
-                <ItemsDispenser />
-            </MainContainer>
+            <ItemsDispenser />
             <Footer />
         </div>
     );
