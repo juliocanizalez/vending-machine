38cdabc30713cf14c49dd7dda0b754aa4ef30066
Julio Eduardo Canizalez Salinas <juliocanizalez@outlook.com>
2021-03-13 23:40:57 -0600

    :hammer: Add container

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header.jsx';
+import '../assets/styles/App.scss';
 
 const App = () => {
     return (
         <div className='App'>
-            
+            <Header />
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
