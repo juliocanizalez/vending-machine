5bef1638fb3a071d1e3e95b6b1c171c5502c4511
Julio Eduardo Canizalez Salinas <juliocanizalez@outlook.com>
2021-03-15 01:27:37 -0600

    :hammer: Updates on get year method

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 import '../assets/styles/components/Footer.scss';
 
 const Footer = () => {
-    let date = new Date();
-    const year = date.getFullYear();
+    let year = new Date().getFullYear();
     return (
         <footer>
             <p className="text-center footer-text">&#169; {year} Made by Julio Canizalez</p>
